eadaa8a2e5731bd084b518493e71d0cd517b3a4d
EthanEriksen <13228370+EthanEriksen@users.noreply.github.com>
2024-06-14 09:57:45 -0400

    Get character count

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_word_count(text)
+    chars_dict = get_chars_dict(text)
     print(f"The text contains {num_words} words.")
+    print(chars_dict)
 
 
 def get_word_count(text):
@@ -13,6 +15,21 @@ def get_word_count(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def get_chars_dict(text):
+    lowered_text = text.lower()
+    char_dict = {}
+
+    for char in lowered_text:
+
+        if char in char_dict:
+            char_dict[char] += 1
+
+        else:
+            char_dict[char] = 1
+
+    return char_dict
 
 
 main()
