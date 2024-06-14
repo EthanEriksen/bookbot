a2c227864ab26a47d79c3045c9a897925c371fca
EthanEriksen <13228370+EthanEriksen@users.noreply.github.com>
2024-06-14 10:10:07 -0400

    add character count report

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     chars_dict = get_chars_dict(text)
-    print(f"The text contains {num_words} words.")
-    print(chars_dict)
+    sorted_chars_list = chars_dict_to_sorted_list(chars_dict)
+    print(f"The text contains {num_words} words.\n")
+    print("=====Begin character count report=====")
+
+    for entry in sorted_chars_list:
+        if entry["char"].isalpha():
+            print(f"The letter {entry['char']} was found {entry['count']} times.")
+
+    print("==============End report==============")
 
 
 def get_word_count(text):
@@ -30,6 +37,25 @@ def get_chars_dict(text):
             char_dict[char] = 1
 
     return char_dict
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def chars_dict_to_sorted_list(chars_dict):
+    chars_sorted_list = []
+    for char in chars_dict:
+        entry_dict = {
+            "char": char,
+            "count": chars_dict[char]
+        }
+
+        chars_sorted_list.append(entry_dict)
+
+    chars_sorted_list.sort(reverse=True, key=sort_on)
+    
+    return chars_sorted_list
 
 
 main()
